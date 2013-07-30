c842935de6c3c9158ab6ea520a36131bc9cfde5e
godgunman <godgunman@gmail.com>
2013-07-30 11:47:33 +0800

    add barcode scanner

---
+++ BarcodeScanner/src/com/example/barcodescanner/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.barcodescanner;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	private static final String BARCODE_SCANNER_ACTION = "com.google.zxing.client.android.SCAN";
+	private static final int BARCODE_SCANNER_CODE = 0;
+
+	private Button button;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		button = (Button) findViewById(R.id.scanButton);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				openScanner();
+			}
+		});
+	}
+
+	private boolean checkInstall() {
+		Intent intent = new Intent();
+		intent.setAction(BARCODE_SCANNER_ACTION);
+		List<ResolveInfo> result = getPackageManager().queryIntentActivities(
+				intent, PackageManager.MATCH_DEFAULT_ONLY);
+		return result.size() != 0;
+	}
+
+	protected void openScanner() {
+		if (checkInstall() == false) {
+			return;
+		}
+		Intent intent = new Intent();
+		intent.setAction(BARCODE_SCANNER_ACTION);
+		intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+		startActivityForResult(intent, BARCODE_SCANNER_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == BARCODE_SCANNER_CODE && resultCode == RESULT_OK) {
+			Log.d("debug", "done");
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
