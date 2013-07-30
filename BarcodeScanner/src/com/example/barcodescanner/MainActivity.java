cedd1041021c80de0442c5ed5c7cbebeb38d0ce6
godgunman <godgunman@gmail.com>
2013-07-30 12:24:58 +0800

    add download file method

--- BarcodeScanner/src/com/example/barcodescanner/MainActivity.java
+++ BarcodeScanner/src/com/example/barcodescanner/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.barcodescanner;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -28,10 +36,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		resultTextView = (TextView) findViewById(R.id.resultTextView);
 		formatTextView = (TextView) findViewById(R.id.formatTextView);
-		
+
 		button = (Button) findViewById(R.id.scanButton);
 		button.setOnClickListener(new OnClickListener() {
 			@Override
@@ -51,6 +59,7 @@ public class MainActivity extends Activity {
 
 	protected void openScanner() {
 		if (checkInstall() == false) {
+			downloadApk();
 			return;
 		}
 		Intent intent = new Intent();
@@ -59,12 +68,38 @@ public class MainActivity extends Activity {
 		startActivityForResult(intent, BARCODE_SCANNER_CODE);
 	}
 
+	private void downloadApk() {
+		String url = "https://zxing.googlecode.com/files/BarcodeScanner4.4.apk";
+		try {
+			URL apkUrl = new URL(url);
+			URLConnection connection = apkUrl.openConnection();
+			InputStream is = connection.getInputStream();
+			FileOutputStream fos = openFileOutput("BarcodeScanner4.4.apk",
+					Context.MODE_PRIVATE);
+
+			byte buffer[] = new byte[1024];
+			int size;
+			while ((size = is.read(buffer)) != -1) {
+				fos.write(buffer, 0, size);
+			}
+
+			File file = new File("BarcodeScanner4.4.apk");
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == BARCODE_SCANNER_CODE && resultCode == RESULT_OK) {
 			String result = data.getStringExtra("SCAN_RESULT");
 			String format = data.getStringExtra("SCAN_RESULT_FORMAT");
-			
+
 			resultTextView.setText(result);
 			formatTextView.setText(format);
 		}
