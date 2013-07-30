75fdd8dc55b62d89fdedb6cc512cecce66126fed
godgunman <godgunman@gmail.com>
2013-07-30 10:08:22 +0800

    update

--- IntentEx/src/com/example/intentex/MainActivity.java
+++ IntentEx/src/com/example/intentex/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.intentex;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,20 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void clickPhoneButton(View view) {
+		Uri uri = Uri.parse("tel:886233663366");
+		Intent intent = new Intent(Intent.ACTION_CALL, uri);
+		startActivity(intent);
+	}
+
+	public void clickWebButton(View view) {
+
+	}
+
+	public void clickEmailButton(View view) {
+
 	}
 
 }
